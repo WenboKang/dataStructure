e274859d9b6e1545a5612420c4c5561d55f9e809
kwb <939068087@qq.com>
2020-05-20 17:03:51 +0800

    运行示例

--- src/com/shangguigu/myQueue/ArrayQueueDemo1.java
+++ src/com/shangguigu/myQueue/ArrayQueueDemo1.java
@@ -20,7 +20,7 @@ public class ArrayQueueDemo1 {
             System.out.println("s(show) : 显示队列");
             System.out.println("e(exit) : 退出程序");
             System.out.println("a(add) :  添加数据到队列");
-            System.out.println("g(get) :  从队列去除数据");
+            System.out.println("g(get) :  从队列取出数据");
             System.out.println("h(head) : 查看队列头部数据");
 
             key = scanner.next().charAt(0);
